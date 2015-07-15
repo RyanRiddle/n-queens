0e0126a582a7eadfc5aa4facee5f7447be471328
Ryan <rcri222@gmail.com>
2015-07-14 20:46:53 -0400

    added a function board-fails-p to test a board represented as a list of 8 queen positions.  added more tests.  one is failing.  not sure why.  it doesn't fail when i run it in the repl.

--- src/eight_queens/core.clj
+++ src/eight_queens/core.clj
@@ -1,7 +1,7 @@
 (ns eight-queens.core
   (:gen-class))
 
-(defn reachablep [q1 q2]
+(defn reachable-p [q1 q2]
 	(let [x1 (first q1)
 		  x2 (first q2)
           y1 (last  q1)
@@ -11,9 +11,22 @@
         	(= y1 y2)
             (= (Math/abs (- x1 x2)) (Math/abs (- y1 y2))))))
 
+(defn board-fails-p [positions]
+	(if (< (count positions) 2)
+		false
+		(let [q1 (first positions)
+			  others (rest positions)]
+			(or
+				(reduce 
+					(fn [result q2]
+						(or result (reachable-p q1 q2))) 
+						false
+						others)
+				(board-fails-p others)))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   
-	(reachablep '(1 1) '(5 2)))	
+	(reachable-p '(1 1) '(5 2)))	
--- test/eight_queens/core_test.clj
+++ test/eight_queens/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [eight-queens.core :refer :all]))
 
-(deftest test-reachablep
-  (testing "Looks like reachablep cannot tell if one queen reaches another"
-    (reachablep '(0 0) '(0 1))))
+(deftest test-reachable-p-returns-true
+  (testing "Looks like reachable-p cannot tell if one queen reaches another"
+    (is (reachable-p '(0 0) '(0 1)) true)))
+
+(deftest test-reachable-p-returns-false
+	(testing "reachable-p says two queens that cannot reach each other can"
+		(is (reachable-p '(0 0) (1 7)) false)))
+
+(deftest test-board-fails-p-returns-true
+	(testing "board-fails-p returned false when it should have returned true"
+		(is (board-fails-p '((0 0) (1 7) (2 1) (1 3))) true)))
+
+(deftest test-board-fails-p-returns-false
+	(testing "board-fails-p returned true when it should have returned false"
+		(is (board-fails-p '((0 0) (1 7) (2 5))) false)))
